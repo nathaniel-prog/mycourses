ab3e6e1890bb14d4ea75d57b61e9c99c992fda13
nathaniel-prog <66939923+nathaniel-prog@users.noreply.github.com>
2021-05-20 16:54:39 +0300

    some change

--- geoloc2.py
+++ geoloc2.py
@@ -4,11 +4,17 @@ import requests
 
 my_url= requests.get('https://ipinfo.io/')
 
-
+my_url2= requests.get(f'https://ipinfo.io/Jerusalem')
 
 respond= my_url.json()
 
-print(my_url.json())
+print(respond)
 
-print(respond['city'])
+#print(my_url2.json())
+
+print(respond['region'])
+
+
+def come(*args , **kwargs):
+	return args 
 
